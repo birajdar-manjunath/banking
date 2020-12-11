ffe45f357842bbbd468da9f571731b5ab3ab9c04
birajdar-manjunath <birajdarmanjunath@gmail.com>
2020-12-11 22:55:21 +0530

    added xyxz

--- src/packg/xyz.java
+++ src/packg/xyz.java
@@ -2,9 +2,9 @@ package packg;
 
 public class xyz {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("i hate you");
 	}
 
 }
